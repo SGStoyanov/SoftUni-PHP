77e6e4c23fc52dfa1d68fbd736816816e16cc6cc
Stoyanov <stoyanov.vn@gmail.com>
2014-08-17 11:29:53 +0300

    Problem 4 rewritten

--- Homework02-PHPForms/04.HTMLTagsCounter.php
+++ Homework02-PHPForms/04.HTMLTagsCounter.php
@@ -8,41 +8,54 @@ Hint: You may use sessions. Use an array to store all valid HTML5 tags. -->
 
 <?php session_start() ?>
 <!DOCTYPE html>
+
+<html>
 <head>
-    <meta charset="UTF-8">
-    <title>Problem 4 - HTML Tags Counter</title>
+    <title>Tag Counter</title>
 </head>
 <body>
     <section>
-        <form method="get" action="">
-            <label for="tag">Enter HTML tags:</label>
-            <br /><br />
-            <input type="text" name="tag" id="tag">
-            <input type="submit" name="submit" value="Submit">
+        <p>Enter HTML Tag or a blank line to reset the session:</p>
+
+        <form action="" method="post">
+            <input type="text" name="tags" id="tag-box"/>
+            <input type="submit" name="submitBtn" id="sub"/>
+            <br/>
+            <div id="result">
+                <?php
+                $validTags = array('a', 'abbr', 'acronym', 'address', 'applet', 'area', 'b', 'base', 'basefont',
+                    'bdo', 'bgsound', 'big', 'blockquote', 'blink', 'body', 'br', 'button', 'caption', 'center', 'cite',
+                    'code', 'col', 'colgroup', 'dd', 'dfn', 'del', 'dir', 'dl', 'div', 'dt', 'embed', 'em', 'fieldset',
+                    'font', 'form', 'frame', 'frameset', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'head', 'hr', 'html', 'iframe',
+                    'img', 'input', 'ins', 'isindex', 'i', 'kbd', 'label', 'legend', 'li', 'link', 'marquee', 'menu' ,
+                    'main', 'meta', 'noframe', 'noscript', 'optgroup', 'option', 'ol', 'p', 'pre', 'q', 's', 'samp',
+                    'section', 'script', 'select', 'small', 'span', 'strike', 'strong', 'style', 'sub', 'sup', 'table',
+                    'td', 'th', 'tr', 'tbody', 'textarea', 'tfoot', 'thead', 'title', 'tt', 'u', 'ul', 'var');
+                $count = 0;
+                if(isset($_POST['tags'])) {
+                    if($_POST['tags'] == '') {
+                        if(isset($_SESSION['count'])) {
+                            unset($_SESSION['count']);
+                        }
+                    }
+                    if(in_array($_POST['tags'], $validTags)) {
+                        if(isset($_SESSION['count'])) {
+                            $_SESSION['count']++;
+                        }
+                        else {
+                            $_SESSION['count'] = 0;
+                        }
+                        echo '<h1>' . "Valid HTML tag!" . '<br />' .
+                            "Score: " . $_SESSION['count'] . '</h1>';
+                    }
+                    else {
+                        echo '<h1>' . "Invalid HTML tag!" . '<br />' .
+                             "Score: " . $_SESSION['count'] . '</h1>';
+                    }
+                }
+                ?>
+            </div>
         </form>
     </section>
 </body>
 </html>
-
-<?php
-$pattern1 = 'abbr|address|area|article|aside|audio|base|bdi|bdo|blockquote|body|br|button|canvas|caption|cite|code|col|colgroup|command|datalist|dd|del|details|dfn|dir|div|dl|dt|em|embed|fieldset|figcaption|figure|footer|form|h1|h2|h3|h4|h5|h6|head|header|hgroup|hr|html|iframe|img|input|ins|kbd|keygen|label|legend|li|link|map|mark|menu|meta|meter|nav|noscript|object|ol|optgroup|option|output|param|pre|progress|rp|rt|ruby|samp|script|section|select|small|source|span|strong|style|sub|summary|sup|table|tbody|td|textarea|tfoot|th|thead|time|title|tr|track|ul|var|video|wbr';
-$pattern2 = 'a|q|b|i|p|s|u';
-$matches = '';
-$tags = $_GET['tag'];
-if (isset($_GET['submit']) && strlen($tags) > 0) {
-    if (!isset($_SESSION['count'])) {
-        $_SESSION['count'] = 0;
-    } else {
-        if (strlen($tags) == 1 && preg_match("($pattern2)", $tags, $matches)) {
-            echo '<h1 style="margin: 5">' . 'Valid HTML tag!' . '</h1>';
-            $_SESSION['count']++;
-        } elseif (strlen($tags) > 1 && preg_match("($pattern)", $tags, $matches)) {
-            echo '<h1 style="margin: 5">' . 'Valid HTML tag!' . '</h1>';
-            $_SESSION['count']++;
-        } else {
-            echo '<h1>' . 'Invalid HTML tag!' . '</h1>';
-        }
-    }
-    echo '<h1 style="margin: 0">' . 'Score: ' . $_SESSION['count'] . '</h1>';
-}
-?>
